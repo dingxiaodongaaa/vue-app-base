1a997dd7db19390f9c6a8443131576012a348ab1
dingxd <dingxiaodongaaa@163.com>
2020-06-29 07:58:54 +0800

    加eslint

--- webpack.common.js
+++ webpack.common.js
@@ -3,64 +3,58 @@ const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: 'js/bundle.js',
-        path: path.join(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                use: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader', 
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10 * 1024,
-                        esModule: false,
-                        name: 'img/[name].[contenthash:8].[ext]'
-                    },
-                }
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new webpack.DefinePlugin({
-            BASE_URL: JSON.stringify('./')
-        }),
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'app',
-            template: './public/index.html'
-        }),
-        new CopyWebpackPlugin([
-            'public'
-        ])
+  entry: './src/main.js',
+  output: {
+    filename: 'js/bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /dist/],
+        use: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /dist/],
+        use: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10 * 1024,
+            esModule: false,
+            name: 'img/[name].[contenthash:8].[ext]'
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      BASE_URL: JSON.stringify('./')
+    }),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'app',
+      template: './public/index.html'
+    })
+  ]
 }
